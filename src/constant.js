cb07bdfebfb1054a1185b53fb4f4952dbdb53c80
Long Tran <s3755614@rmit.edu.vn>
2022-06-12 17:02:33 +0700

    Update constants

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-const DEPLOYED_UPLOAD_URL = 'http://localhost:8080/api/uploads'
+const DEPLOYED_UPLOAD_URL = 'http://annotationnode-env.eba-iv5i9cmp.us-west-2.elasticbeanstalk.com/api/uploads'
 const PLACEHOLDER = "http://annotationnode-env.eba-iv5i9cmp.us-west-2.elasticbeanstalk.com"
 export default DEPLOYED_UPLOAD_URL
