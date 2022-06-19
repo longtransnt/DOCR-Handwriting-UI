0951fb532ce4591658ba4ed831ec0cd4d4767edd
Nhan <s3687637@rmit.edu.vn>
2022-06-20 00:15:31 +0700

    handle width

--- src/components/OriginalView.js
+++ src/components/OriginalView.js
@@ -14,26 +14,26 @@ function OriginalView(props) {
   const selectedImage = props.selectedImage
   const [preview, setPreview] = useState()
 
-    // create a preview as a side effect, whenever selected file is changed
-    useEffect(() => {
-        if (!selectedImage) {
-            setPreview(undefined)
-            return
-        }
-        const objectUrl = URL.createObjectURL(selectedImage)
-        setPreview(objectUrl)
-        // free memory when ever this component is unmounted
-        return () => URL.revokeObjectURL(objectUrl)
-    }, [selectedImage])
+  // create a preview as a side effect, whenever selected file is changed
+  useEffect(() => {
+      if (!selectedImage) {
+          setPreview(undefined)
+          return
+      }
+      const objectUrl = URL.createObjectURL(selectedImage)
+      setPreview(objectUrl)
+      // free memory when ever this component is unmounted
+      return () => URL.revokeObjectURL(objectUrl)
+  }, [selectedImage])
 
-    const togglePopup = () => {
-      setIsOpen(!isOpen);
-    }
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
 
-    const onImgLoad = ({ target: img }) => {
-      const { offsetHeight, offsetWidth } = img;
-      console.log(offsetHeight, offsetWidth);
-    };
+  const onImgLoad = ({ target: img }) => {
+    const { offsetHeight, offsetWidth } = img;
+    console.log(offsetHeight, offsetWidth);
+  };
     
 
   return (
@@ -51,17 +51,16 @@ function OriginalView(props) {
           content={<>
             <div>
               <ImageMapping
-                onLoad={onImgLoad({ target: { image2 } })}
                 active={true}
-                width={500} imgWidth={1480} // imgWidth: original image width
-                src={image}
+                width={500} imgWidth={572} // imgWidth: original image width
+                src={image2}
                 map={{
                     name: 'my-map',
                     areas: [
-                      // { shape: 'rect', coords: [68, 1468, 462, 1401] },
-                      { shape: 'rect', coords: [78, 1220, 192, 1290] },
-                      { shape: 'rect', coords: [856, 1258, 1026, 1368] },
-                      { shape: 'rect', coords: [868, 1594, 1152, 1512] }, // [top-left-X,top-left-Y,bottom-right-X,bottom-right-Y] Top left x,y(x_min, y_max); Bottom right x,y (x_max, y_min)
+                      { shape: 'rect', coords: [68, 1468, 462, 1401] },
+                      // { shape: 'rect', coords: [78, 1220, 192, 1290] },
+                      // { shape: 'rect', coords: [856, 1258, 1026, 1368] },
+                      // { shape: 'rect', coords: [868, 1594, 1152, 1512] }, // [top-left-X,top-left-Y,bottom-right-X,bottom-right-Y] Top left x,y(x_min, y_max); Bottom right x,y (x_max, y_min)
                     ]
                 }}
               />
