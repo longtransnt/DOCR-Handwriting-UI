15bc6d273e091a55cd50e83b7f4086a3072ecc2e
Long Tran <s3755614@rmit.edu.vn>
2022-06-21 14:41:49 +0700

    Add checkbox to select whether upload original image or normal image

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Dropzone from "react-dropzone-uploader";
 import 'react-dropzone-uploader/dist/styles.css'
 
 function Upload({ fetchUploads }) {
+  const [originalUpload, setOriginalUpload] = useState(false)
+  
   const getUploadParams = ({ file }) => {
+
+    var fixed_path = 'http://annotationnode-env.eba-iv5i9cmp.us-west-2.elasticbeanstalk.com'
+    var path = ''
+    if (originalUpload === true) 
+      path = fixed_path + '/api/originals'
+    else
+      path = fixed_path + '/api/uploads'
+
     const body = new FormData()
     body.append('image', file)
-    return {
-      url: 'http://localhost:8080/api/originals',
-      body
-    };
+      return {
+        url: path,
+        body
+      };
+    
   }
 
   const handleSubmit = (files, allFiles) => {
@@ -29,6 +40,12 @@ function Upload({ fetchUploads }) {
           dropzone: { minHeight: 200, maxHeight: 250 }
         }}
       />
+      <label>
+        <input type="checkbox"
+          defaultChecked={originalUpload}
+          onChange={() => setOriginalUpload(!originalUpload)}
+        />Upload Original Image
+      </label>
     </div>
   );
 }
