01689778c21cb42d318cd8a3d02609d259a9ce89
Long Tran <s3755614@rmit.edu.vn>
2022-06-21 14:04:00 +0700

    Modify load coordinates button

--- src/components/Coordinate.js
+++ src/components/Coordinate.js
@@ -5,23 +5,25 @@ import 'react-dropzone-uploader/dist/styles.css'
 import { Button } from 'react-bootstrap';
 function Coordinate() {
     const getData=()=>{
-        let obj = myJson[1];
-        console.log(obj);
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                image_name: obj.image_name,
-                original_image_name:obj.original_image_name,
-                max_x:obj.max_x,
-                max_y:obj.max_y,
-                min_x:obj.min_x,
-                min_y:obj.min_y
-            })
-        };
-        fetch('http://localhost:8080/api/coordinate', requestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data));
+        for (let i = 0; i < myJson.length; i++) {
+            let obj = myJson[i];
+            console.log(obj);
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 
+                    image_name: obj.image_name,
+                    original_image_name:obj.original_image_name,
+                    max_x:obj.max_x,
+                    max_y:obj.max_y,
+                    min_x:obj.min_x,
+                    min_y:obj.min_y
+                })
+            };
+            fetch('http://localhost:8080/api/coordinate', requestOptions)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
       }
       
   return (
