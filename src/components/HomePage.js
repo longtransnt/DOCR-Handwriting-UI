788e857af460d5bfd60c7bf07c6d6b38f0728574
Nhan <s3687637@rmit.edu.vn>
2022-08-03 10:23:52 +0700

    refactor css files

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
- 
-function HomePage (){
-    return <h1>Welcome to the world of Geeks!</h1>
+import image1 from '../test_image/21,000927 (9).jpg';
+import image2 from '../test_image/2021_12_30 09_55 Office Lens (23).jpg';
+
+function HomePage() {
+    return (
+        <div className='home-gallery'>
+            <img className='original-image' src={image1}></img>
+            <img className='original-image' src={image2}></img>
+        </div>
+    )
 }
  
 export default HomePage;
