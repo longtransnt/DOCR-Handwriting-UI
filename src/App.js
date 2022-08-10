b46942b92c96e77dc5f81fffc808c1a6a641a92a
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-08-10 21:50:16 +0700

    annotation by original id

--- src/App.js
+++ src/App.js
@@ -2,27 +2,19 @@ import "./App.css";
 
 import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AnnotationPage from "./components/AnnotationPage";
 import HomePage from "./components/HomePage";
-import { createBrowserHistory } from "history";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage></HomePage>} />
-          <Route path="/annotation" element={<AnnotationPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>} />
+        <Route path="/annotation" element={<AnnotationPage />} />
+        <Route path="/annotation/:id" element={<AnnotationPage />} />
+      </Routes>
+    </div>
   );
 }
 
