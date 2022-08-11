b7e75bbb544c9681700869044415baf4282980ed
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-08-11 10:51:37 +0700

    connect

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AnnotationPage from "./components/AnnotationPage";
 import HomePage from "./components/HomePage";
+import { createBrowserHistory } from "history";
 
+const customHistory = createBrowserHistory({ forceRefresh: true });
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage></HomePage>} />
-        <Route path="/annotation" element={<AnnotationPage />} />
-        <Route path="/annotation/:id" element={<AnnotationPage />} />
-      </Routes>
+      <Router history={customHistory}>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>} />
+          <Route path="/annotation" element={<AnnotationPage />} />
+          <Route path="/annotation/:id" element={<AnnotationPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
