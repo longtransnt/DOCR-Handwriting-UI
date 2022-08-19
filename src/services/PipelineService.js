f6aa47dd3f6bd43d628387f493f45bb4a333a9ef
Long Tran <longtran.se@gmail.com>
2022-08-19 20:30:55 +0700

    Add routes for pipeline and display image

--- src/services/PipelineService.js
+++ src/services/PipelineService.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { PIPELINE_CHECK_DIR_URL } from "../constant";
+
+let STATIC_LIST_ENDPOINT = 'http://localhost:5000/get-static-list/';
+let STATIC_DISPLAY = 'http://localhost:5000/display/';
+
 let axiosConfig = {
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
@@ -14,3 +18,36 @@ async function checkIfdirectoryExist(path) {
   );
   return isExist;
 }
+
+async function getInputImageList() {
+  const url = `http://localhost:5000/get-input-list`;
+    const imagelist = await axios.get(url, axiosConfig);
+    return imagelist;
+}
+
+async function getListOfImageNames(path, category) {
+  if (category === undefined) {
+    const url = `http://localhost:5000/get-static-list/${path}`;
+    const imagelist = await axios.get(url, axiosConfig);
+    return imagelist;
+  }
+  else {
+    const url = `http://localhost:5000/get-static-list/${path}/${category}`;
+    const imagelist = await axios.get(url, axiosConfig);
+    return imagelist;
+  }
+}
+
+function getImageUrl(path, name, category) {
+  if (category === undefined) {
+    const url = `http://localhost:5000/display-output/${path}/${name}.jpg`;
+    return url;
+  }
+  else {
+    const url = `http://localhost:5000/display-sub-output/${path}/${category}/${name}.jpg`;
+    // console.log(url)
+    return url;
+  }
+}
+
+export default { checkIfdirectoryExist, getInputImageList, getImageUrl, getListOfImageNames };
